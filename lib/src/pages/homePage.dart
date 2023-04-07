f59afeec31a62ebc5ff0dd10ba1c67cdfce9d9d4
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-04-07 23:35:21 +0700

    Try to make horizontal scroll

--- lib/src/pages/homePage.dart
+++ lib/src/pages/homePage.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +10,24 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         automaticallyImplyLeading: false,
         title: Text("DEEMARK Burger"),
+      ),
+      body: Container(
+        color: Colors.amberAccent,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              child: Image.asset('assets/images/b1.jpg'),
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              child: Image.asset('assets/images/b2.jpg'),
+            ),
+          ],
+        ),
       ),
     );
   }
