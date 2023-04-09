816a3d76c60ce932fff2e273443fcf7edbf05975
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-04-09 21:52:02 +0700

    Finished with the slider

--- lib/src/pages/homePage.dart
+++ lib/src/pages/homePage.dart
@@ -1,5 +1,13 @@
-
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  'assets/images/b1.jpg',
+  'assets/images/b2.jpg',
+  'assets/images/b3.jpg',
+  'assets/images/b4.jpg',
+  'assets/images/b5.jpg',
+];
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +21,35 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.black,
         automaticallyImplyLeading: false,
       ),
+      body: Container(
+        color: Colors.grey,
+        width: double.infinity,
+        padding: EdgeInsets.all(15),
+        child: Stack(
+          children: [
+            CarouselSlider(
+              items: genarateImagesTile(),
+              options: CarouselOptions(
+                enlargeCenterPage: true,
+                autoPlay: false,
+              ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
+}
+
+
+List<Widget> genarateImagesTile() {
+  return imgList
+      .map((i) => ClipRect(
+              child: Image.asset(
+            i,
+            fit: BoxFit.cover,
+            width: 1000,
+          )))
+      .toList();
 }
